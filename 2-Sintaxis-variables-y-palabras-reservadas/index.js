acbdfa319dd7b24799a54e330c8b6eff7739ffdf
Rios <rios25866@gmail.com>
2023-04-24 14:05:06 -0400

    1.0

---
+++ 2-Sintaxis-variables-y-palabras-reservadas/index.js
@@ -0,0 +1,14 @@
+const me = {
+    name: "Armando",
+    age: 21,
+    developer: true,
+    date: new Date("2001-09-21"),
+    bookFavorite: {
+        title: "Hambriento",
+        author: "Nach",
+        date: new Date("2016-11-10"),
+        ulr: "https://www.planetadelibros.com/libro-hambriento/216992"
+    }
+}
+
+console.log(me);

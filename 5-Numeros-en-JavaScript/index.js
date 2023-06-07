84a929f93266ed39f8dfaa02ae219104fe354027
Armando Rios <rios25866@gmail.com>
2023-06-07 04:06:06 -0400

    1.0

---
+++ 5-Numeros-en-JavaScript/index.js
@@ -0,0 +1,17 @@
+// Seccion 5 numbers
+
+const a = "235"
+let b = parseInt(a)
+const c =5
+console.log(typeof(b));
+console.log(b + c);
+console.log(a + c);
+
+let d = 19;
+let g = 0;
+console.log(d / g);
+
+let numero = Number("20")
+let numero2 = 13;
+console.log(numero + numero2);
+console.log(typeof b);
